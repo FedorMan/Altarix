1c32b6360b5016cf1fd16e303c1de97e12d7f577
FedorMan <fedor.leontev@gmail.com>
2017-10-15 12:56:43 +0400

    Задокументирован контроллер департаментов.

--- src/main/java/com/company/controll/controller/DepartmentController.java
+++ src/main/java/com/company/controll/controller/DepartmentController.java
@@ -4,16 +4,20 @@ import com.company.controll.entity.Department;
 import com.company.controll.entity.Employe;
 import com.company.controll.model.DepartamentInformation;
 import com.company.controll.repository.DepartmentRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static javax.servlet.http.HttpServletResponse.*;
 
 @Controller
 @RequestMapping("api/department")
@@ -22,30 +26,48 @@ public class DepartmentController {
     @Autowired
     private DepartmentRepository departmentRepository;
 
-    //Создает новый департамент с учетом следующих правил:
-    //1)только у Верхнего в иерархии департамента нет родительского
-    //2)в системе не может быть двух одинаково названных департаментов
+    @ApiOperation(value = "Создать департамент",
+            notes = "Только у Верхнего в иерархии департамента нет родительского." +
+                    "В системе не может быть двух одинаково названных департаментов.",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_CREATED, message = "Департамент создан"),
+            @ApiResponse(code = SC_CONFLICT, message = "Главный департамент уже имеется или департамент с таким именем уже создан")
+    })
     @RequestMapping(path = "/create", method = RequestMethod.POST)
-    public ResponseEntity<String> create(RequestEntity<Department> requestEntity) {
+    public ResponseEntity create(RequestEntity<Department> requestEntity) {
         Department department = requestEntity.getBody();
-        if (!validateHeadDepartment(department)) return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).build();
-        if (!validateEqualsDepartment(department.getName())) return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        if (!validateHeadDepartment(department) || !validateEqualsDepartment(department.getName()))
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         departmentRepository.save(department);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    //изменяет наименование департамента с учетом того что в системе нет департаментов с одинаковыми названиями
+    @ApiOperation(value = "Изменить название департамента",
+            notes = "Изменяет наименование департамента с учетом того что в системе " +
+                    "нет департаментов с одинаковыми названиями",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, message = "Департамент изменен"),
+            @ApiResponse(code = SC_CONFLICT, message = "Департамент с таким именем уже создан")
+    })
     @RequestMapping(path = "/update", method = RequestMethod.GET)
-    public ResponseEntity<String> update(@RequestParam(value = "id") long id, @RequestParam(value = "name") String name) {
+    public ResponseEntity update(@RequestParam(value = "id") long id, @RequestParam(value = "name") String name) {
         if (!validateEqualsDepartment(name)) return ResponseEntity.status(HttpStatus.CONFLICT).build();
         departmentRepository.getOne(id).setName(name);
         departmentRepository.flush();
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    //удаляет указанный департамент, при условии, что в нем нет сотрудников
+    @ApiOperation(value = "Удалить департамент",
+            notes = "Удаляет указанный департамент, при условии, что в нем нет сотрудников",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, message = "Департамент удален"),
+            @ApiResponse(code = SC_CONFLICT, message = "В департаменте есть сотрудники")
+    })
     @RequestMapping(path = "/delete", method = RequestMethod.DELETE)
-    public ResponseEntity<String> delete(@RequestParam(value = "id") long id) {
+    public ResponseEntity delete(@RequestParam(value = "id") long id) {
         if (departmentRepository.getOne(id).getEmployes().isEmpty()) {
             departmentRepository.delete(id);
             departmentRepository.flush();
@@ -54,21 +76,42 @@ public class DepartmentController {
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
-    //Просмотр сведений о департаменте.
-    // Должна быть выдана информация о наименовании департамента,
-    // дате создания, руководителе департамента и количестве сотрудников департамента.
+    @ApiOperation(value = "Просмотр сведений о департаменте. ",
+            notes = "Выдает информацию о наименовании департамента, дате создания," +
+                    " руководителе департамента и количестве сотрудников департамента.",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Информация о департаменте"),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
     @RequestMapping(path = "/information", method = RequestMethod.GET)
-    public @ResponseBody DepartamentInformation getInformation(@RequestParam(value = "id") long id) {
-        Department department = departmentRepository.getOne(id);
-        Optional<Employe> mainEmploye = department.getEmployes().stream().filter(employe -> employe.isMain()).findFirst();
-        DepartamentInformation departamentInformation = new DepartamentInformation(department.getName(), department.getCreateBy(), mainEmploye.get(), department.getEmployes().size());
-        return departamentInformation;
+    public ResponseEntity getInformation(@RequestParam(value = "id") long id) {
+        try {
+            Department department = departmentRepository.getOne(id);
+            Optional<Employe> mainEmploye = department.getEmployes().stream().filter(employe -> employe.isMain()).findFirst();
+            DepartamentInformation departamentInformation = new DepartamentInformation(department.getName(), department.getCreateBy(),
+                    mainEmploye.isPresent() ? mainEmploye.get() : null,
+                    department.getEmployes().size());
+            return new ResponseEntity(departamentInformation, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity("Департамент не найден", HttpStatus.NOT_FOUND);
+        }
     }
 
-    //Предоставление информации о департаментах, находящихся в
-    // непосредственном подчинении данного департамента (на уровень ниже).
-    @RequestMapping(path = "/downinformation", method = RequestMethod.GET)
-    public @ResponseBody List<DepartamentInformation> getDownInformation(@RequestParam(value = "id") long id) {
+    @ApiOperation(value = "Просмотр сведений о подчиненных департаментах. ",
+            notes = "Выдает информацию о департаментах, находящихся в непосредственном подчинении",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Список департаментов находящихся в подчинении"),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
+    @RequestMapping(path = "/subordinates", method = RequestMethod.GET)
+    public ResponseEntity getSubordinateDepartments(@RequestParam(value = "id") long id) {
+        try {
+            Department department = departmentRepository.getOne(id);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity("Департамент не найден", HttpStatus.NOT_FOUND);
+        }
         List<Department> departments = departmentRepository.findAllByParentDepartmentId(id);
         List<DepartamentInformation> departamentInformations = new ArrayList<>();
         for (Department department : departments) {
@@ -78,56 +121,104 @@ public class DepartmentController {
                     mainEmploye.isPresent() ? mainEmploye.get() : null,
                     department.getEmployes().size()));
         }
-        return departamentInformations;
+        return new ResponseEntity(departamentInformations, HttpStatus.OK);
     }
 
-    //Предоставление информации о всех департаментах, находящихся в подчинении данного департамента
-    // (все подчиненные департаменты. Для головного департамента - это все остальные департаменты).
-    @RequestMapping(path = "/downallinformation", method = RequestMethod.GET)
-    public @ResponseBody List<DepartamentInformation> getDownAllInformation(@RequestParam(value = "id") long id) {
+    @ApiOperation(value = "Просмотр сведений о всех подчиненных департаментах. ",
+            notes = "Выдает информацию о всех департаментах, находящихся подчинении",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Список департаментов находящихся в подчинении"),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
+    @RequestMapping(path = "/allsubordinates", method = RequestMethod.GET)
+    public ResponseEntity getAllSubordinateDepartments(@RequestParam(value = "id") long id) {
+        try {
+            Department department = departmentRepository.getOne(id);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity("Департамент не найден", HttpStatus.NOT_FOUND);
+        }
         List<DepartamentInformation> departamentInformations = new ArrayList<>();
         findAllInformationAboutDepartment(id, departamentInformations);
-        return departamentInformations;
+        return new ResponseEntity(departamentInformations, HttpStatus.OK);
     }
 
-    //Перенос департамента. Задание другого департамента, куда будет входить данный департамент.
+    @ApiOperation(value = "Перенос департамента",
+            notes = "Задает другой департамент в который будет входить указанный",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Департамент перенесен"),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
     @RequestMapping(path = "/transposition", method = RequestMethod.GET)
-    public ResponseEntity<String> transposition(@RequestParam(value = "id") long id, @RequestParam(value = "parentId") long parentId){
-        Department department = departmentRepository.getOne(id);
-        Department parentDepartment = departmentRepository.getOne(parentId);
-        department.setParentDepartment(parentDepartment);
-        departmentRepository.flush();
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    //Поиск департамента по наименованию.
-    @RequestMapping(path = "/findbyname", method = RequestMethod.GET)
-    public @ResponseBody Department findByName(@RequestParam(value = "name") String name){
-        return departmentRepository.findByName(name);
-    }
-
-    //Получение информации о фонде заработной платы департамента
-    // (сумма зарплат всех сотрудников департамента).
-    @RequestMapping(path = "/salarydepartment", method = RequestMethod.GET)
-    public @ResponseBody Double getAllSalary(@RequestParam(value = "id") long id){
-        Department department = departmentRepository.getOne(id);
-        return department.getEmployes().stream().mapToDouble(value -> value.getSalary()).sum();
-    }
-
-    //Получение информации о всех вышестоящих департаментах данного департамента.
-    @RequestMapping(path = "upallinformation", method = RequestMethod.GET)
-    public @ResponseBody List<DepartamentInformation> getUpAllInformation(@RequestParam(value = "id") long id){
-        List<DepartamentInformation> departamentInformations = new ArrayList<>();
-        Department department = departmentRepository.getOne(id);
-        while (department.getParentDepartment()!=null){
-            department = department.getParentDepartment();
-            Optional<Employe> mainEmploye = department.getEmployes().stream().filter(employe -> employe.isMain()).findFirst();
-            departamentInformations.add(new DepartamentInformation(department.getName(),
-                    department.getCreateBy(),
-                    mainEmploye.isPresent() ? mainEmploye.get() : null,
-                    department.getEmployes().size()));
+    public ResponseEntity transposition(@RequestParam(value = "id") long id, @RequestParam(value = "parentId") long parentId) {
+        try {
+            Department department = departmentRepository.getOne(id);
+            Department parentDepartment = departmentRepository.getOne(parentId);
+            department.setParentDepartment(parentDepartment);
+            departmentRepository.flush();
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity("Департамент не найден", HttpStatus.NOT_FOUND);
         }
-        return departamentInformations;
+    }
+
+    @ApiOperation(value = "Поиск департамента по наименованию",
+            notes = "Возвращает департамент с заданным наименованием",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Департамент найден"),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
+    @RequestMapping(path = "/find", method = RequestMethod.GET)
+    public ResponseEntity findByName(@RequestParam(value = "name") String name) {
+        Department department = departmentRepository.findByName(name);
+        if (department == null) return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity(department, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Информация о заработной плате департамента",
+            notes = "Возвращает суммарную заработную пллату департамента",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Заработная плата департамента"),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
+    @RequestMapping(path = "/salary", method = RequestMethod.GET)
+    public ResponseEntity getSalaryDepartment(@RequestParam(value = "id") long id) {
+        try {
+            Department department = departmentRepository.getOne(id);
+            double sallary = department.getEmployes().stream().mapToDouble(value -> value.getSalary()).sum();
+            return new ResponseEntity(sallary, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity("Департамент не найден", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ApiOperation(value = "Просмотр сведений о вышестоящих департаментах. ",
+            notes = "Выдает информацию о всех вышестоящих департаментах данного департамент",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = SC_OK, response = DepartamentInformation.class, message = "Список вышестоящих департаментов."),
+            @ApiResponse(code = SC_NOT_FOUND, message = "Департамент не найден")
+    })
+    @RequestMapping(path = "superordinate", method = RequestMethod.GET)
+    public ResponseEntity getSuperordinateDepartments(@RequestParam(value = "id") long id) {
+        try {
+            List<DepartamentInformation> departamentInformations = new ArrayList<>();
+            Department department = departmentRepository.getOne(id);
+            while (department.getParentDepartment() != null) {
+                department = department.getParentDepartment();
+                Optional<Employe> mainEmploye = department.getEmployes().stream().filter(employe -> employe.isMain()).findFirst();
+                departamentInformations.add(new DepartamentInformation(department.getName(),
+                        department.getCreateBy(),
+                        mainEmploye.isPresent() ? mainEmploye.get() : null,
+                        department.getEmployes().size()));
+            }
+            return new ResponseEntity(departamentInformations, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity("Департамент не найден", HttpStatus.NOT_FOUND);
+        }
     }
 
     //проверить является ли добавляемый департамент верхним в иерархии
