f279a9f369b54a5a3a0a06993032323bda7d9057
FedorMan <fedor.leontev@gmail.com>
2017-10-16 14:22:01 +0400

    Добавлен SalaryDepartmentRepository.

---
+++ src/main/java/com/company/controll/repository/SalaryDepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.company.controll.repository;
+
+import com.company.controll.entity.SalaryDepartment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaryDepartmentRepository extends JpaRepository<SalaryDepartment,Long>{
+}
